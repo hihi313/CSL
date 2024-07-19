b35f52cce8a9cc356c1f59cca218b2c511a547a7
hih313 <31838134+hihi313@users.noreply.github.com>
2024-07-19 11:03:15 +0800

    validated csl, remove invalid vol.

--- clean_name.py
+++ clean_name.py
@@ -5,31 +5,47 @@ import re
 
 # Create a regex pattern to match years from 1600 to 2099
 year_ptrn = re.compile(r"\s*\b(1[6-9]\d{2}|20[0-9]{2})\b\s*")
+# Create a regex pattern to match abbreviations (3 or more uppercase letters)
 abbr_ptrn = re.compile(r"\s*\(\s*\b([A-Z]{3,})\b\s*\)\s*")
 
+def is_int(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
 
 def process_bib_file(filename):
+    # Define a middleware for decoding LaTeX
     layers = [
-        m.LatexDecodingMiddleware(),
+        # m.LatexDecodingMiddleware(),
     ]
+    # Parse the .bib file with the middleware
     library = bibtexparser.parse_file(filename, append_middleware=layers)
 
+    # Iterate over each entry in the library
     for entry in library.entries:
-        for k in ("journaltitle", "booktitle"):
+        # Check if "volume" is not a number then delete the field
+        if "volume" in entry and not is_int(entry["volume"]):
+            del entry["volume"]
+        # Check for "journaltitle" and "booktitle" keys in the entry
+        for k in ("journaltitle", "booktitle", "journal"):
             if k in entry:
                 v = entry[k]
+                # Search for year and abbreviation in the value
                 match = re.search(year_ptrn, v)
                 year = match.group(1) if match else None
                 match = re.search(abbr_ptrn, v)
                 abbr = match.group(1) if match else None
 
+                # Remove the year and abbreviation from the value
                 v2 = year_ptrn.sub("", v)
                 entry[k] = abbr_ptrn.sub("", v2)
+                # print(f"date:  {entry["date"]}")
     return library
 
-
 if __name__ == "__main__":
-
+    # Define command line arguments
     parser = argparse.ArgumentParser(description="Process a .bib file.")
     parser.add_argument(
         "input_file", type=str, help="The path to the .bib file to process."
@@ -42,5 +58,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    # Process the .bib file and write the output to a new file
     lib = process_bib_file(args.input_file)
-    bibtexparser.write_file("output.bib", lib)
+    print(f"Entry count: {len(lib.entries)}")
+    layers = [
+        # m.LatexEncodingMiddleware(),
+    ]
+    bibtexparser.write_file("output.bib", lib, append_middleware=layers)
